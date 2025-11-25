61bca2d9c6efbc92ca8a1d2b71831586fc2246f3
Cesar <Vilpandocesar7@gmail.com>
2025-11-25 18:36:31 -0300

    feito

--- database/migrations/2025_11_22_190025_create_produtos_table.php
+++ database/migrations/2025_11_22_190025_create_produtos_table.php
@@ -8,12 +8,16 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('folder_id'); // <-- chave estrangeira
             $table->string('nome');
             $table->text('descricao')->nullable();
-            $table->decimal('preco', 10, 2);
+           
             $table->integer('estoque')->default(0);
             $table->integer('quantidade')->default(0); 
+            $table->date('data_vencimento')->nullable();   // <-- novo campo
+    $table->integer('dias_restantes')->nullable(); // <-- novo campo
             $table->timestamps();
+            $table->foreign('folder_id')->references('id')->on('folders')->onDelete('cascade');
         });
     }
 
