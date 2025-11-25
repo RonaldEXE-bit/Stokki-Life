7f18bdabf9c3ab496b5bbce35e7cce5f6fd0c34d
Cesar <Vilpandocesar7@gmail.com>
2025-11-25 17:38:02 -0300

    arrumado

---  entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Aplica migrations automaticamente
-php artisan migrate --force
-
-# Sobe servidor Laravel na porta esperada pelo Render/Railway
-php artisan serve --host=0.0.0.0 --port=8080
-
